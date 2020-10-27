729a66b874ea78930f05d26fdbe7d241ad3c66a8
gandarezt <gandarez.t@gmail.com>
2020-10-27 23:00:10 +0000

    Initial commit

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet("Get")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "ola", "ola again" };
+        }
+
+        [HttpGet("Get")]
+        public ActionResult<string> GetName()
+        {
+            return "Gandarez";
+        }
+
+    }
+}
---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.TEST
+{
+    public class UnitTest1
+    {
+
+
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnsMyÑame()
+        {
+            var returnValue = controller.GetName();
+            Assert.Equal("Gandarez", returnValue.Value);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
